d284acad043f41179b302d444392a6c5097415a3
Droomii <dowoo2594@naver.com>
2020-11-14 19:08:05 +0900

    201114 lv3 섬 연결하기 효율

--- lv3/섬_연결하기/Solution.java
+++ lv3/섬_연결하기/Solution.java
@@ -1,57 +1,42 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42861
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
-	public int solution(int n, int[][] costs) {
-		List<Conn> connections = Arrays.stream(costs).map(Conn::new).sorted().collect(Collectors.toList());
-		Set<Integer> connected = new HashSet<>();
-		int cost = 0;
-		connected.addAll(connections.get(0).nodes);
-		cost += connections.get(0).cost;
-		connections.remove(0);
-		
-		while(connected.size() < n) {
-			Iterator<Conn> it = connections.iterator();
-			while(it.hasNext()) {
-				Conn conn = it.next();
-				if(connected.containsAll(conn.nodes)) continue;
-				
-				if(connected.removeAll(conn.nodes)) {
-					cost += conn.cost;
-					it.remove();
-					connected.addAll(conn.nodes);
-					break;
-				}
-				
-			}
-		}
-		return cost;
-	}
+    public int solution(int n, int[][] costs) {
+    	Arrays.sort(costs, (a, b)->a[2]-b[2]);
+    	List<int[]> costList = new ArrayList<>();
+    	for(int[] cost : costs) {
+    		costList.add(cost);
+    	}
+    	
+    	
+    	Set<Integer> connected = new HashSet<>();
+    	int[] start = costList.remove(0);
+    	int answer = start[2];
+    	
+    	connected.add(start[0]);
+    	connected.add(start[1]);
+    	
+    	while(connected.size()<n) {
+    		Iterator<int[]> it = costList.iterator();
+    		while(it.hasNext()) {
+    			int[] next = it.next();
+    			if(connected.contains(next[0]) && connected.contains(next[1])) {
+    				it.remove();
+    				continue;
+    			}
+    			if(connected.contains(next[0]) || connected.contains(next[1])) {
+    		    	connected.add(next[0]);
+    		    	connected.add(next[1]);
+    				answer += next[2];
+    				it.remove();
+    				break;
+    			}
+    		}
+    	}
+        return answer;
+    }
 }
 
-class Conn implements Comparable<Conn>{
-	public Set<Integer> nodes;
-	public int cost;
-	
-	public Conn(int[] cost) {
-		this.nodes = new HashSet<Integer>();
-		nodes.add(cost[0]);
-		nodes.add(cost[1]);
-		this.cost = cost[2];
-	}
-	@Override
-	public int compareTo(Conn o) {
-		return this.cost - o.cost;
-	}
-	@Override
-	public String toString() {
-		return "Conn [nodes=" + nodes + ", cost=" + cost + "]";
-	}
-	
-	
+
 }
