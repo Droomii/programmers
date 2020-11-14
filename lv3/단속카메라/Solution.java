4a503f67f307bf0dc67b9da65471e2ced7dc2fe2
Droomii <dowoo2594@naver.com>
2020-11-14 19:37:00 +0900

    201114 lv3 단속카메라 효율

--- lv3/단속카메라/Solution.java
+++ lv3/단속카메라/Solution.java
@@ -1,47 +1,24 @@
-// https://programmers.co.kr/learn/courses/30/lessons/42884
-
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
+    public static int min = -30000;
+    public static int max = 30000;
     public int solution(int[][] routes) {
-    	if(routes.length==1) {
-    		return 1;
-    	}
-    	Queue<Car> q = new PriorityQueue<Car>();
-    	for(int[] route : routes) {
-    		q.add(new Car(route[0], route[1]));
-    	}
-    	int cnt = 1;
-    	Car currentCar = q.poll();
-    	int start = currentCar.start;
-    	int end = currentCar.end;
-    	while(!q.isEmpty()) {
-    		currentCar = q.poll();
-    		start = start < currentCar.start ? currentCar.start : start;
-    		end = end > currentCar.end ? currentCar.end : end;
-    		if(start>end) {
-    			cnt++;
-    			start = currentCar.start;
-    			end = currentCar.end;
-    		}
-    	}
-    	return cnt;
+        Arrays.sort(routes, (a,b)->(a[0] - b[0]));
+        int enterCnt = min;
+        int exitCnt = max;
+        int answer = 1;
+        for(int[] route : routes){
+            if(exitCnt < route[0]){
+                enterCnt = route[0];
+                exitCnt = route[1];
+                answer++;
+            }else{
+            enterCnt = enterCnt > route[0] ? enterCnt : route[0];
+            exitCnt = exitCnt < route[1] ? exitCnt : route[1];
+            }
+        }
+        
+        return answer;
     }
-}
-
-class Car implements Comparable<Car>{
-	public int start;
-	public int end;
-	
-	public Car(int start, int end) {
-		super();
-		this.start = start;
-		this.end = end;
-	}
-	@Override
-	public int compareTo(Car o) {
-		return this.start - o.start;
-	}
-	
 }
