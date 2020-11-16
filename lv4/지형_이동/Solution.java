ac37bc319de976d9ceb53556e9c3dceb274862b7
Droomii <36333478+Droomii@users.noreply.github.com>
2020-11-16 10:18:53 +0900

    201116 lv4 지형이동 수정

--- lv4/지형_이동/Solution.java
+++ lv4/지형_이동/Solution.java
@@ -1,50 +1,51 @@
 // https://programmers.co.kr/learn/courses/30/lessons/62050
-
-import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
 
 class Solution {
-	static int[] rowDir = {-1, 0, 1, 0};
-	static int[] colDir = {0, 1, 0, -1};
+	static int[][] urdl = {{-1,0},{0,1},{1,0},{0,-1}};
+	static int N;
     public int solution(int[][] land, int height) {
-    	int len = land.length;
-    	int fieldCnt = len*len;
-    	int totalCost = 0;
-    	boolean[][] visited = new boolean[len][len];
-    	int connected = 1;
-    	// {col, row, height}
-    	Queue<int[]> q = new ArrayDeque<>();
-    	PriorityQueue<int[]> tooHighQ = new PriorityQueue<>((o1, o2) -> o1[2]-o2[2]);
-    	q.add(new int[]{0, 0, land[0][0]});
-    	while(connected < fieldCnt) {
-    		while(!q.isEmpty()) {
-        		int[] field = q.poll();
-        		if(visited[field[0]][field[1]]) continue;
-        		visited[field[0]][field[1]] = true;
-        		connected++;
-        		for(int i = 0; i < 4; i++) {
-        			try {
-        				int nextRow = field[0]+rowDir[i];
-        				int nextCol = field[1]+colDir[i];
-        				int diff = Math.abs(field[2]-land[nextRow][nextCol]); 
-        				if(diff > height){
-        					tooHighQ.add(new int[] {nextRow, nextCol, diff});
-        				}else {
-        					q.add(new int[] {nextRow, nextCol, land[nextRow][nextCol]});
-        				}
-        			}catch(ArrayIndexOutOfBoundsException e) {}
-        		}
-        	}
-    		while(!tooHighQ.isEmpty()) {
-    			int[] border = tooHighQ.poll();
-    			if(!visited[border[0]][border[1]]) {
-    				q.add(new int[] {border[0], border[1], land[border[0]][border[1]]});
-    				totalCost += border[2];
-    				break;
+    	int fieldCnt = land.length * land.length;
+    	N = land.length;
+    	Set<Integer> visited = new HashSet<>();
+    	int cost = 0;
+    	
+    	Queue<int[]> q = new PriorityQueue<>((a, b)->a[1]-b[1]);
+    	q.add(new int[] {coordToNum(0, 1), Math.abs(land[0][0]-land[0][1])});
+    	q.add(new int[] {coordToNum(1, 0), Math.abs(land[0][0]-land[1][0])});
+    	visited.add(0);
+    	while(visited.size() < fieldCnt) {
+    		int[] cross = q.poll();
+    		if(visited.contains(cross[0])) continue;
+    		
+    		visited.add(cross[0]);
+    		int[] coord = numToCoord(cross[0]);
+    		int diff = cross[1];
+    		
+    		if(diff > height)
+    			cost += diff;
+    		
+    		for(int i = 0; i < 4; i++) {
+    			try {
+    				int[] to = {coord[0]+urdl[i][0], coord[1]+urdl[i][1]};
+    				int nextDiff =Math.abs(land[coord[0]][coord[1]]-land[to[0]][to[1]]);
+    				int[] next = {coordToNum(to[0], to[1]), nextDiff};
+    				q.add(next);
+    			}catch(ArrayIndexOutOfBoundsException e) {
+    				continue;
     			}
     		}
     	}
-        return totalCost;
+        return cost;
+    }
+    
+    public static int coordToNum(int row, int col) {
+    	return row*N + col;
+    }
+    public static int[] numToCoord(int num) {
+    	return new int[] {num / N, num % N};
     }
 }
